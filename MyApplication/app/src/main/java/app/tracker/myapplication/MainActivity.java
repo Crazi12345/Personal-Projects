32e5931ca04780ff13259ed88764ad702fc6b9a7
Patrick <Patrickbjerre@outlook.dk>
2022-03-16 12:49:41 +0100

    Auto stash before merge of "main" and "origin/main"

--- MyApplication/app/src/main/java/app/tracker/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/app/tracker/myapplication/MainActivity.java
@@ -1,8 +1,15 @@
 package app.tracker.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.*;
+import android.view.ViewConfiguration;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Fragment first = new FirstFragment();
+        Fragment second = new SecondFragment();
+        Button frag1 = findViewById(R.id.Frag1);
+       frag1.setOnClickListener(new OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               setContentView(R.layout.fragment_first);
+           }
+       });
+
     }
 }
